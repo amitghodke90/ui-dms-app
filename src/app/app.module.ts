44b6407a3c62ab2c0f461cb9a1902387697979a2
dhananjaybhirud <dhananjay.b@mobinexttech.com>
2019-01-08 10:42:33 +0530

    - bootstrap and some components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -9,16 +11,28 @@ import {CommonModule} from '@angular/common';
 import {FileSelectDirective} from 'ng2-file-upload';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './components/header/header.component';
+import { CreateTeamComponent } from './components/create-team/create-team.component';
+import { InviteMemberComponent } from './components/invite-member/invite-member.component';
+import { UploadDocumentComponent } from './components/upload-document/upload-document.component';
+import { SearchDocumentComponent } from './components/search-document/search-document.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FileSelectDirective
+    FileSelectDirective,
+    HeaderComponent,
+    CreateTeamComponent,
+    InviteMemberComponent,
+    UploadDocumentComponent,
+    SearchDocumentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    NgbModule,
+    AngularFontAwesomeModule,
     CommonModule,
     HttpClientModule,
     AppRoutingModule
---
+++ src/app/components/invite-member/invite-member.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invite-member',
+  templateUrl: './invite-member.component.html',
+  styleUrls: ['./invite-member.component.scss']
+})
+export class InviteMemberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
